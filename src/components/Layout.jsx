847bb3015660d0baa173815505990898b8fc90dc
farhan <farhnn.r@gmail.com>
2022-04-10 17:11:34 +0700

    context login functional

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
+import { AuthContext } from "../utils/Context";
 
 const Layout = (props) => {
-  return (
-    <div style={{ background: "#F3F3F3", minHeight: "100vh" }}>
-      <Navbar />
-      {props.children}
-    </div>
-  );
+  const auth = useContext(AuthContext);
+
+  if (auth[0].status == "success") {
+    alert("success login");
+    return (
+      <div style={{ background: "#F3F3F3", minHeight: "100vh" }}>
+        <Navbar />
+        {props.children}
+      </div>
+    );
+  } else {
+    // console.log(auth[0].status);
+    alert("login first!");
+    window.location.href = "/login";
+  }
 };
 
 export default Layout;
